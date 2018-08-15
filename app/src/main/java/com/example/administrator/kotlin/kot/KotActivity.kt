0214358702f4914095bcfbd147eff94c7016fb6b
luzhiquan <13384077876@163.com>
2018-08-15 09:10:53 +0800

    初始化代码

---
+++ app/src/main/java/com/example/administrator/kotlin/kot/KotActivity.kt
@@ -0,0 +1,14 @@
+package com.example.administrator.kotlin.kot
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Administrator on 2018/6/18.
+ */
+class KotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
